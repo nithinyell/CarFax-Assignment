fea62e853750a36c468a7d6e4fa0f47a72d6f0cb
Nithin <nithinyell@gmail.com>
2020-08-27 12:51:17 +0530

    Initial Repo Set Up

---
+++ CarFaxTests/CarFaxTests.swift
@@ -0,0 +1,34 @@
+//
+//  CarFaxTests.swift
+//  CarFaxTests
+//
+//  Created by Nithin on 27/08/20.
+//  Copyright © 2020 Nithin. All rights reserved.
+//
+
+import XCTest
+@testable import CarFax
+
+class CarFaxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
